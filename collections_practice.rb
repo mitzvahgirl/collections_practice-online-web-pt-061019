d6f9053b8abccde947e47c5635b2303e8296d1b3
Amy Fogelman <abfnj1@gmail.com>
2019-06-12 01:06:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(arr)
+  array.sort
+end  
+  
+def sort_array_desc(arr)
+    array.sort {|x,y| y <=> x }
+end
 
+def sort_array_char_count(arr)
+   array.sort {|left, right| left.length <=> right.length} e
+  
+def swap_elements(arr)
+  array[1], array[2] = array[2], array[1]
+  array
+end  
+
+def reverse_array(arr)
+   array.reverse
+end   
+
+def kesher_maker(arr) 
+ 
+ 
+def find_a(arr)  
+    array.select do |word|
+    word.chr == "a"
+  end
+end
+
+  
+def sum_array(arr)   
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+  
+def add_s(arr)
+   array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
